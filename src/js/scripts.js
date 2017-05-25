707affd42a57202367109aaf9e7796450d3b16e8
ruslan <rendzhi@mail.ru>
2017-05-25 13:00:54 +0700

    fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -45,6 +45,48 @@ $(document).ready(function(){
         toggleBody();
         $(this).toggleClass('active');
     });
+
+    // Слайдер;
+    $('div.main-slides div.items').owlCarousel({
+        //navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        lazyEffect: false,
+        //autoPlay: 5000,
+        navigation:false,
+        singleItem:true
+    });
+    //адаптированный карусель
+    $(".owl-carousel").owlCarousel({
+        //autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 8,
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : false, // betweem 900px and 601px
+        itemsTablet: false, //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+    //Слайд партнеры
+    $("#owl-parter").owlCarousel({
+        autoPlay: 6000, //Set AutoPlay to 3 secon
+        singleItem:true,
+        //slideSpeed : 300,
+        // paginationSpeed : 400,
+        pagination: true
+    });
+
+    //Слайд партнеры
+    $("#owl-parter-cop").owlCarousel({
+        autoPlay: 6000, //Set AutoPlay to 3 secon
+        singleItem:true,
+        //slideSpeed : 300,
+        // paginationSpeed : 400,
+        pagination: false,
+        navigationText : true,
+        navigation: true
+
+    });
+
+
 });
 console.log('mobila-- Ok');
 console.log('версия jQuery ' + jQuery.fn.jquery);
